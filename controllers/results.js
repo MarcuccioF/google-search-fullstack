7984c1617498707384c7b5db03ba29190661716f
MarcuccioF <95623153+MarcuccioF@users.noreply.github.com>
2022-01-27 16:59:47 +0700

    added dynamic views + filter function + partial result

--- controllers/results.js
+++ controllers/results.js
@@ -2,8 +2,65 @@
 const express = require('express')
 const router = express.Router()
 // Create POST controller
+
 router.post('/', (req, res) => {
-  res.render('results')
+  let results = [
+    {
+      title: 'JS tutorials',
+      description: 'The best JavaScript tutorials in the galaxy!',
+      url: 'https://www.w3schools.com',
+      links: [
+        {
+          title: 'JS for Beginners',
+          url: 'https://www.w3schools.com/js'
+        },
+        {
+          title: 'JS for the Web',
+          url: 'https://www.w3schools.com/js'
+        }
+      ]
+    },
+    {
+      title: 'JavaScript',
+      description: 'The best language!',
+      url: 'https://www.developer.mozilla.org',
+      links: [
+        {
+          title: 'JS for life',
+          url: 'https://www.w3schools.com/js'
+        },
+        {
+          title: 'JS for the DarkWeb',
+          url: 'https://www.w3schools.com/js'
+        }
+      ]
+    },
+    {
+      title: 'JS pazz',
+      description: 'The best song  in the galaxy!',
+      url: 'https://www.pinodaniele.com',
+      links: [
+        {
+          title: 'Pino for Beginners',
+          url: 'https://www.w3schools.com/js'
+        },
+        {
+          title: 'Pino for the Web',
+          url: 'https://www.w3schools.com/js'
+        }
+      ]
+    }
+  ]
+  results = results.filter(
+    e =>
+      e.title.includes(req.body.research) ||
+      e.description.includes(req.body.research)
+  )
+  console.log(results)
+  // res.render('results', { results })
+  res.render('results', { results })
+
+  console.log(req.body.research)
 })
 // Export module
 module.exports = router
